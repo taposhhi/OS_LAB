40d82356fc3351dc20378a090bf9469bde7a5b94
taposhhi <137192266+taposhhi@users.noreply.github.com>
2026-05-06 19:15:47 +0600

    Update and rename round_robin.cpp to SJF.cpp

---
+++ SJF.cpp
@@ -0,0 +1,64 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    string process[n];
+    int burst[n];
+    int waiting[n];
+
+    cout << "Enter process name and burst time: ";
+    cout << endl;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> process[i] >> burst[i];
+    }
+
+
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = i+1; j < n; j++)
+        {
+            if(burst[j] < burst[i])
+            {
+                swap(burst[i], burst[j]);
+                swap(process[i], process[j]);
+            }
+        }
+    }
+
+    waiting[0] = 0;
+    for(int i = 1; i < n; i++)
+    {
+        waiting[i] = waiting[i-1] + burst[i-1];
+    }
+
+
+    int time = 0;
+    cout << time << " ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << process[i] << " ";
+        time += burst[i];
+        cout << time << " ";
+    }
+
+
+    float total_wait = 0;
+    for(int i = 0; i < n; i++)
+    {
+        total_wait += waiting[i];
+    }
+
+    float avg_wait = total_wait / n;
+
+    cout << "\n\nAverage Waiting Time: " << avg_wait << endl;
+
+    return 0;
+}
