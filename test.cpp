73c470a3af7917cd16ca0a34142dc63f4072151e
taposhhi <137192266+taposhhi@users.noreply.github.com>
2026-05-18 22:26:40 +0600

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+  int n = 5;
+
+  cout << "this is another proc" << endl;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
